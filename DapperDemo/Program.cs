e0c279634e2c7926be99cf977d48948cff6d5f56
wenjie <connyoursis@163.com>
2019-05-21 16:28:50 +0800

    v1.0.5引入dapper

---
+++ DapperDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DapperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RolePermission rp = new RolePermission
+            {
+                Id=1000,
+                MenuId = 7,
+                Permission = "test!!",
+                RoleId = 1
+            };
+            //引入Dapper
+            IDbConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=EasyAdmin;User ID=sa;pwd=123123123");
+            var insertSql = "insert into RolePermission (MenuId,Permission,RoleId) values(@MenuId,@Permission,@RoleId)";
+
+            connection.Execute(insertSql, rp);
+
+            var querySql = "select * from RolePermission";
+            //无参数查询，返回列表，带参数查询和之前的参数赋值法相同。
+            var list =connection.Query<RolePermission>(querySql).ToList();
+
+            foreach(var s in list)
+            {
+                Console.WriteLine(s.Permission);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
